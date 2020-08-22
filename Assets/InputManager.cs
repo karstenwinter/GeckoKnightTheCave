565c5ea235127f098ac6bc40f353336a02d2bc34
karsten <brox.p@web.de>
2020-08-22 13:50:15 +0200

    update

---
+++ Assets/InputManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class JoyInputController
+{
+    internal static float m_forward;
+    internal static bool m_jump;
+    internal static bool m_crouch;
+    internal static bool m_attackPrimary;
+    internal static bool m_attackSecondary;
+}
+public static class InputManager
+{
+
+    public static float Forward()
+    {
+        float kb = Input.GetAxis("Horizontal");
+
+        kb += JoyInputController.m_forward;
+        kb = Mathf.Clamp(kb, -1f, 1f);
+
+        return kb;
+    }
+
+    public static bool Jump()
+    {
+        bool kb = Input.GetButtonDown("Jump") || JoyInputController.m_jump;
+        //private joy
+
+        return kb;
+    }
+
+    public static bool Crouch()
+    {
+        bool kb = Input.GetButton("Fire1") || JoyInputController.m_crouch;
+        //private joy
+
+        return kb;
+    }
+
+    public static bool AttackPrimary()
+    {
+        bool kb = Input.GetButtonDown("Fire2") || JoyInputController.m_attackPrimary;
+        //private joy
+
+        return kb;
+    }
+
+    public static bool AttackSecondary()
+    {
+        bool kb = Input.GetButtonDown("Fire3") || JoyInputController.m_attackSecondary;
+        //private joy
+
+        return kb;
+    }
+
+    public static bool JumpPressForGrab()
+    {
+        bool kb = Input.GetButton("Jump") || JoyInputController.m_jump;
+
+        return kb;
+    }
+}
--- Assets/LevelOnDemand.cs
+++ Assets/LevelOnDemand.cs
@@ -3,28 +3,29 @@ using UnityEditor.Experimental.AssetImporters;
 using System.IO;
 using UnityEngine.Tilemaps;
 
-[ScriptedImporter(1, "tmx")]
-public class TilemapImporter : ScriptedImporter
+//[ScriptedImporter(1, "tmx")]
+public class LevelOnDemand : MonoBehaviour
 {
-    public float m_Scale = 1;
-    public GameObject withTilemap;
-    public int startX, startY, width, height;
-    public bool dummy;
+    //public float m_Scale = 1;
+    //public GameObject withTilemap;
+    //public int offset, offset2;
+    //public bool dummy;
 
-    public override void OnImportAsset(AssetImportContext ctx)
+    public void Start()
     {
-        var t = withTilemap.GetComponent<Tilemap>();
-        var parent = new GameObject();
-        var layerText = File.ReadAllText(ctx.assetPath)
+        Render(transform.position, "", null);
+    }
+
+    public void Render(Vector3 pos, string layer, Tilemap target)
+    {
+        /*
+        string[] txt = File.ReadAllText()
         .Replace("\r", "")
         .Split('\n');
-        string[] txt = layerText;
-        //var position = JsonUtility.FromJson<Vector3>(txt);
+        
         var tilemap = parent.AddComponent<Tilemap>();
-        //var tilemapR = 
-        parent.AddComponent<TilemapRenderer>();
-        //var tilemapC = 
-        parent.AddComponent<TilemapCollider2D>();
+        var tilemapR = parent.AddComponent<TilemapRenderer>();
+        var tilemapC = parent.AddComponent<TilemapCollider2D>();
         var y = -1;
         foreach (string line in txt)
         {
@@ -33,38 +34,20 @@ public class TilemapImporter : ScriptedImporter
             foreach (string c in line.Split(','))
             {
                 x++;
-                if (startX <= x && x <= width && startY <= y && y <= height && c != "" && c != "0")
+                if (c != "" && c != "0")
                 {
                     //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                     var position = new Vector3Int(x, -y, 0);
-                    long value;
-                    if (!long.TryParse(c, out value))
-                    {
-                        Debug.LogError("c => error " + c);
-                    }
+                    var value = int.Parse(c);
                     //if (value < 10)
                     //{
                     ///}
                     var tx = value % 8;
                     var ty = value / 8;
-                    /*if (value < 9)
-                    {
-                        value--;
-                    }*/
-
-                    /*if (value > 9)
-                    {
-                        ty += offset;
-                    }
-                    else
-                    {
-                        value--;
-                        ty += offset2;
-                    }*/
-                    //Debug.Log("y" + y + "x" + x + ": v" + value + "=> ty" + ty + "tx" + tx);
+                    Debug.Log("y" + y + "x" + x + ": v" + value + "=> ty" + ty + "tx" + tx);
                     //tx -= 1;
-                    var tile = t.GetTile(new Vector3Int((int)(tx - 1), (int)-ty, 0));
-                    tilemap.SetTile(position - new Vector3Int(startX, startY, 0), tile);
+                    var tile = t.GetTile(new Vector3Int(tx - 1, -ty, 0));
+                    tilemap.SetTile(position, tile);
                     //cube.name="y"+y+"x"+x;
                     //cube.transform.parent=parent.transform;
                     //  cube.transform.position = position;
@@ -89,6 +72,6 @@ public class TilemapImporter : ScriptedImporter
 
         // Assets that are not passed into the context as import outputs must be destroyed
         var tempMesh = new Mesh();
-        DestroyImmediate(tempMesh);
+        DestroyImmediate(tempMesh);*/
     }
 }
