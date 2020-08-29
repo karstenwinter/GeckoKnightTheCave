dd099f6f750f50b7cf8b317633c452f2414b4a49
Karsten <brox.p@web.de>
2020-08-29 17:47:02 +0200

    behaviour

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    Vector3 startPos;
+    public float speed = 2f;
+    public float patrolWidth = 8f;
+    bool headingRight;
+    float dx;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        dx += (headingRight ? speed : -speed) * Time.deltaTime;
+        transform.position = startPos + new Vector3(dx, 0, 0);
+
+        if (headingRight && dx > patrolWidth)
+        {
+            headingRight = false;
+        }
+
+        if (!headingRight && dx < -patrolWidth)
+        {
+            headingRight = true;
+        }
+    }
+}
